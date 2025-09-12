111f3dcc0d73d9331b7edb049d00bcdc89809e68
Rachatanawee <rachatanawee@csithai.com>
2025-09-12 11:29:47 +0700

    effect 1

--- src/app/[locale]/(dashboard)/layout.tsx
+++ src/app/[locale]/(dashboard)/layout.tsx
@@ -1,18 +1,34 @@
+"use client"
+
 import { Header } from "./_components/header"
 import { SidebarNav } from "./_components/sidebar-nav"
+import { motion, AnimatePresence } from "framer-motion"
+import { useRouter } from "next/navigation"
 
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const router = useRouter()
   return (
     <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
       <SidebarNav />
       <div className="flex flex-col">
         <Header />
         <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
-          {children}
+          <AnimatePresence>
+            <motion.div
+              key={router.pathname}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 0.5 }}
+              className="h-full w-full"
+            >
+              {children}
+            </motion.div>
+          </AnimatePresence>
         </main>
       </div>
     </div>
